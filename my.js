ed2550a2f64e819f4055420055948ad63167c3aa
cymun111 <kenyon.westbrook@gmail.com>
2017-04-27 09:47:09 -0700

    Update my.js

--- my.js
+++ my.js
@@ -20,13 +20,27 @@ let template4 = document.querySelector('#corvtemplate');
 var clone = document.importNode(template4.content, true);
 document.getElementById('corv').appendChild(clone);
 
-var cars = $('#carsli li');
-$("#searchform").keyup(function() {
-  var re = new RegExp($(this).val(), "i"); // "i" means it's case-insensitive
-  cars.show().filter(function() {
-      return !re.test($(this).text());
-  }).hide();
-    });
+function myFunction() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchBar");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
 //var priceTemplate = document.querySelector("#priceTemplate");
 //var clone = document.importNode(priceTemplate.content, true);
 //var price = document.getElementsByClassName('price');

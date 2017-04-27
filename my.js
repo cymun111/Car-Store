0c43aaf89c291c6de2f4f58106e55034a287e47d
cymun111 <kenyon.westbrook@gmail.com>
2017-04-27 10:38:20 -0700

    Update my.js

--- my.js
+++ my.js
@@ -1,4 +1,24 @@
 
+function myFunction() {
+  // Declare variables 
+  var input, filter, table, tr, th, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    th = tr[i].getElementsByTagName("th")[0];
+    if (th) {
+      if (th.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
 $(document).ready(function(){
 let template = document.querySelector('#bmwtemplate');
 var clone = document.importNode(template.content, true);
@@ -20,26 +40,7 @@ let template4 = document.querySelector('#corvtemplate');
 var clone = document.importNode(template4.content, true);
 document.getElementById('corv').appendChild(clone);
 
-function myFunction() {
-  // Declare variables 
-  var input, filter, table, tr, th, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    th = tr[i].getElementsByTagName("th")[0];
-    if (th) {
-      if (th.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
 
 //var priceTemplate = document.querySelector("#priceTemplate");
 //var clone = document.importNode(priceTemplate.content, true);
